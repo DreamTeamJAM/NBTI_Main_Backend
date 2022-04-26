36684037eb33581fd3208b73688ce5bc26b8231e
Antonio <tonidiez17@gmail.com>
2022-04-26 11:37:55 +0100

    company

--- src/main/java/com/nbti/backEnd/controllers/CompanyController.java
+++ src/main/java/com/nbti/backEnd/controllers/CompanyController.java
@@ -1,13 +1,16 @@
 package com.nbti.backEnd.controllers;
 
 import java.util.List;
+import java.util.NoSuchElementException;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -53,4 +56,21 @@ public class CompanyController {
 			return new ResponseEntity<>(null, HttpStatus.INTERNAL_SERVER_ERROR);
 		}
 }
+	
+	@DeleteMapping("/company/{id}")
+	  public ResponseEntity<String> eliminarPorId(@PathVariable Long id) {
+		  
+			  return serv.deleteById(id) ? new ResponseEntity<>("deletion succesful", HttpStatus.OK)
+						: new ResponseEntity<>(" Id not in DB", HttpStatus.NOT_FOUND);
+	  }
+	
+	@PutMapping("/company")
+	 public ResponseEntity<Company> updateHost(@RequestBody Company company){
+	
+		try {
+			return new ResponseEntity<>(serv.update(company), HttpStatus.OK);
+		} catch (NoSuchElementException e) {
+			return ResponseEntity.notFound().build();
+		} 
+	}
 }
