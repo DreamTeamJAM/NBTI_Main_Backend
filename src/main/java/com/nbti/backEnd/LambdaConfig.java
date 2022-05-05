d4f68dfea537892e8515c5bf5584f73042d6687d
meskion <manufdh@hotmail.com>
2022-05-05 10:06:23 +0100

    Start Up optimizations

--- src/main/java/com/nbti/backEnd/LambdaConfig.java
+++ src/main/java/com/nbti/backEnd/LambdaConfig.java
@@ -1,29 +1,40 @@
 package com.nbti.backEnd;
 
 import org.springframework.context.annotation.Configuration;
-import org.springframework.context.annotation.Import;
-
-import com.nbti.backEnd.controllers.UserController;
 
 @Configuration
-@Import(UserController.class)
+//@EnableTransactionManagement
+
 public class LambdaConfig {
 
+	
+
+//
 //	@Bean
-//    public StudentController studentController() {
-//         return new StudentController();
-//    }
-//	
+//	@Qualifier(value = "entityManager")
+//	public EntityManager entityManager(EntityManagerFactory entityManagerFactory) {
+//		return entityManagerFactory.createEntityManager();
+//	}
+//
+////	@Bean
+////	public StudentController studentController() {
+////		return new StudentController();
+////	}
+////
 //	@Bean
-//    public UserController userController() {
-//         return new UserController();
-//    }
-//	
+//	public UserController userController() {
+//		return new UserController();
+//	}
+//
+//	@Bean
 //	public UserService userService() {
 //		return new UserServiceImpl();
 //	}
-	
-	
-    
-   
+//
+//	@Bean
+//	public UserRepository userRepository(EntityManager entityManager) {
+//		JpaRepositoryFactory jpaRepositoryFactory = new JpaRepositoryFactory(entityManager);
+//		return jpaRepositoryFactory.getRepository(UserRepository.class);
+//	}
+
 }
--- src/main/java/com/nbti/backEnd/NbtiBackEndApplication.java
+++ src/main/java/com/nbti/backEnd/NbtiBackEndApplication.java
@@ -1,18 +1,40 @@
 package com.nbti.backEnd;
 
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.boot.autoconfigure.jackson.JacksonAutoConfiguration;
 import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
-import org.springframework.context.annotation.Import;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
 
-import com.nbti.backEnd.controllers.UserController;
+//@SpringBootApplication
 
-@SpringBootApplication
+@Configuration
+@EnableAutoConfiguration(exclude={JacksonAutoConfiguration.class})
+@ComponentScan
+//@Import({
+//    DispatcherServletAutoConfiguration.class,
+//    ServletWebServerFactoryAutoConfiguration.class,
+//    ErrorMvcAutoConfiguration.class,
+//    HttpEncodingAutoConfiguration.class,
+//    HttpMessageConvertersAutoConfiguration.class,
+//    JacksonAutoConfiguration.class,
+//    PropertyPlaceholderAutoConfiguration.class,
+//    WebMvcAutoConfiguration.class,
+//    SqlInitializationAutoConfiguration.class,
+//    JpaRepositoriesAutoConfiguration.class,
+//    DataSourceAutoConfiguration.class,
+//    HibernateJpaAutoConfiguration.class,
+//    JpaBaseConfiguration.class,
+//    LambdaConfig.class,
+//})
+
+//@EntityScan("com.nbti.backEnd.model")
+
 public class NbtiBackEndApplication extends SpringBootServletInitializer {
 
 	 // silence console logging
-    @Value("${logging.level.root:OFF}")
+//    @Value("${logging.level.root:OFF}")
     String message = "";
     
 	public static void main(String[] args) {
