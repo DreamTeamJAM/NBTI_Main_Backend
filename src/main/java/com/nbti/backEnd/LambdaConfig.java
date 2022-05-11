1fd3ac11d325014bef5f81223a1293f5e61a76a3
meskion <manufdh@hotmail.com>
2022-05-11 09:58:25 +0100

    enabled global cors configuration

--- src/main/java/com/nbti/backEnd/LambdaConfig.java
+++ src/main/java/com/nbti/backEnd/LambdaConfig.java
@@ -1,13 +1,19 @@
 package com.nbti.backEnd;
 
 import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
 //@EnableTransactionManagement
+@EnableWebMvc
+public class LambdaConfig implements WebMvcConfigurer {
 
-public class LambdaConfig {
-
-	
+	    @Override
+	    public void addCorsMappings(CorsRegistry registry) {
+	        registry.addMapping("/**");
+	    }
 
 //
 //	@Bean
